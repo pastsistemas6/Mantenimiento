506a0d2dee2b6cd91c68242e393ddc5cac0aae10
pastsistemas6 <pastsistemas6@eliteflower.com>
2025-07-03 13:24:11 -0500

    error

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import 'jquery';
-import 'apexcharts';
-import 'datatables.net';
-import 'datatables.net-buttons';
+
+function buildChart(selector, options) {
+    const chart = new ApexCharts(document.querySelector(selector), options);
+    chart.render();
+    return chart;
+}
+
+window.buildChart = window.buildChart || function(selector, options) {
+    const chart = new ApexCharts(document.querySelector(selector), options);
+    chart.render();
+    return chart;
+};
 
 import App from './App.vue'
 import router from './router'
