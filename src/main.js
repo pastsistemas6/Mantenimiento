181479858ac040d72f21059a11b53051bfde0846
pastsistemas6 <pastsistemas6@eliteflower.com>
2025-07-03 11:00:54 -0500

    cambio 2

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ApexCharts from 'apexcharts';
+import $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-buttons';
 
 import App from './App.vue'
 import router from './router'
